1d4e135db58da2fe2d7fea5271a664e6f516adcf
sandeepparimi <158067728+sandeepparimi@users.noreply.github.com>
2024-06-04 18:56:24 +0530

    Update qr-scanner.js

--- src/components/qr-scanner.js
+++ src/components/qr-scanner.js
@@ -1,13 +1,18 @@
 // @ts-nocheck
 import React, { useState, useEffect } from 'react';
+import { Form, Row, Col, Card, Modal, ProgressBar } from 'react-bootstrap';
+import Image from 'next/image';
 import QrReader from 'react-qr-scanner';
 import { Button } from 'react-bootstrap';
 
-const QRScan = (apiData, setApiData) => {
+const QRScan = ({ apiData, setApiData }) => {
     const [selected, setSelected] = useState("certification verification");
     const [startScan, setStartScan] = useState(false);
     const [loadingScan, setLoadingScan] = useState(false);
     const [data, setData] = useState("");
+    const [loginError, setLoginError] = useState('');
+    const [loginSuccess, setLoginSuccess] = useState('');
+    const [show, setShow] = useState(false);
     // const [apiData, setApiData] = useState([]);
 
     const apiUrl = process.env.NEXT_PUBLIC_BASE_URL;
@@ -22,21 +27,21 @@ const QRScan = (apiData, setApiData) => {
         console.log("The data", data);
     }, [startScan]);
 
-    const handleScan = async (scanData) => {
+    const _handleScan = async (scanData) => {
         setLoadingScan(true);
         let scanFailed = true; // Flag to track if the scan failed
         var scanResponse = "No URL found";
 
         // Start a timer for 10 seconds
-        // const timeout = setTimeout(() => {
-        //     if (scanFailed) {
-        //         // If the scan failed after 10 seconds, pass the custom URL to the API
-        //         scanResponse = "Unable to detect QR, Try again with different Verification method";
-        //         setLoadingScan(false);
-        //         setData(scanResponse);
-        //         setStartScan(false);
-        //     }
-        // }, 10000); // 10 seconds in milliseconds
+        const timeout = setTimeout(() => {
+            if (scanFailed) {
+                // If the scan failed after 10 seconds, pass the custom URL to the API
+                scanResponse = "Unable to detect QR, Try again with different Verification method";
+                setLoadingScan(false);
+                setData(scanResponse);
+                setStartScan(false);
+            }
+        }, 10000); // 10 seconds in milliseconds
 
         if (scanData) {
             try {
@@ -56,12 +61,15 @@ const QRScan = (apiData, setApiData) => {
                 if (qrScanResponse.ok) { // Check if response is successful
                     const responseData = await qrScanResponse.json(); // Parse response body as JSON
                     console.log("The response", responseData.data); // Do something with the response data
-                    // setApiData(responseData.data);
+                    setApiData(responseData);
                     scanFailed = false;
                     // clearTimeout(timeout); // Clear the timeout if the API call succeeds before the 10-second timeout
                 } else {
-                    console.error('Error:', qrScanResponse.statusText); // Log error message
+                    const responseData = await qrScanResponse.json(); // Parse response body as JSON
+                    console.error("The response", responseData);
                     scanFailed = true; // Set flag to true if the scan failed
+                    setLoginError( responseData.message || "Unable to scan the QR. Please review and try again.")
+                    setShow(true)
                 }
 
                 setData(scanResponse);
@@ -72,6 +80,79 @@ const QRScan = (apiData, setApiData) => {
             }
         }
         setLoadingScan(false);
+    };
+
+    const handleScan = async (scanData) => {
+        setLoadingScan(true);
+        let scanFailed = true; // Flag to track if the scan failed
+        let scanResponse = "No URL found";
+        let timeout;
+    
+        const startTimeout = () => {
+            timeout = setTimeout(() => {
+                if (scanFailed) {
+                    // If the scan failed after 10 seconds, pass the custom URL to the API
+                    scanResponse = "Unable to detect QR, Try again with different Verification method";
+                    setLoadingScan(false);
+                    setData(scanResponse);
+                    setStartScan(false);
+                }
+            }, 10000); // 10 seconds in milliseconds
+        };
+    
+        const clearExistingTimeout = () => {
+            if (timeout) {
+                clearTimeout(timeout);
+                timeout = null;
+            }
+        };
+    
+        startTimeout();
+    
+        if (scanData) {
+            try {
+                // First API call with QR Scanned data
+                const qrScanResponse = await fetch(`${apiUrl}/api/decode-qr-scan`, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        receivedCode: scanData.text,
+                    }),
+                });
+    
+                console.log(`QR data: ${scanData.text}`);
+                scanResponse = scanData ? scanData.text : scanResponse;
+                if (qrScanResponse.ok) { // Check if response is successful
+                    const responseData = await qrScanResponse.json(); // Parse response body as JSON
+                    console.log("The response", responseData.data); // Do something with the response data
+                    setApiData(responseData);
+                    scanFailed = false;
+                } else {
+                    const responseData = await qrScanResponse.json(); // Parse response body as JSON
+                    console.error("The response", responseData);
+                    setLoginError(responseData.message || "Unable to scan the QR. Please review and try again.")
+                    setShow(true);
+                }
+    
+                clearExistingTimeout(); // Clear the timeout if the API call succeeds or fails
+                setData(scanResponse);
+                setStartScan(false);
+            } catch (error) {
+                console.error("Error during API call:", error);
+                clearExistingTimeout(); // Clear the timeout if there's an error during the API call
+            }
+        } else {
+            clearExistingTimeout(); // Clear the timeout if no scan data is provided
+        }
+    
+        setLoadingScan(false);
+    };
+    
+
+    const handleClose = () => {
+        setShow(false);
     };
 
     const handleError = (err) => {
@@ -127,6 +208,39 @@ const QRScan = (apiData, setApiData) => {
                 {loadingScan && <p>Loading</p>}
                 <div><h4>Response: {data}</h4></div>
             </div>
+            <Modal onHide={handleClose} className='loader-modal text-center' show={show} centered>
+                <Modal.Body className='p-5'>
+                    {loginError !== '' ? (
+                        <>
+                            <div className='error-icon'>
+                                <Image
+                                    src="/icons/close.svg"
+                                    layout='fill'
+                                    objectFit='contain'
+                                    alt='Loader'
+                                />
+                            </div>
+                            <h3 style={{ color: 'red' }}>{loginError}</h3>
+                            <button className='warning' onClick={handleClose}>Ok</button>
+                        </>
+                    ) : (
+                        <>
+                            <div className='error-icon'>
+                                <Image
+                                    src="/icons/check-mark.svg"
+                                    layout='fill'
+                                    objectFit='contain'
+                                    alt='Loader'
+                                />
+                            </div>
+                            <h3 style={{ color: '#198754' }}>{loginSuccess}</h3>
+                            <button className='success' onClick={handleClose}>Ok</button>
+                        </>
+                    )}
+
+
+                </Modal.Body>
+            </Modal>
         </div>
 
     );
