96af746e3bcca33763c2bd7d7371b3bdd6dd4ad2
Mohak Talwar <mohak.talwar@netcomlearning.com>
2025-01-02 18:36:38 +0530

    remove additional tags

--- src/pages/exam-certificate.js
+++ src/pages/exam-certificate.js
@@ -100,27 +100,6 @@ import { SlSocialFacebook, SlSocialLinkedin, SlSocialTwitter } from 'react-icons
     }
     return (
         <>
-          
-           <Head>
-           <title>{title}</title>
-                <meta name="description" content={description} />
-                <meta property="og:title" content={title} />
-                <meta property="og:description" content={description} />
-                <meta property="og:image" content={imageUrl} />
-                <meta property="og:image:width" content="1200" />
-                <meta property="og:image:height" content="630" />
-                <meta property="og:image:type" content="image/png" />
-                <meta property="og:image:secure_url" content={imageUrl} />
-                <meta property="og:url" content={shareUrl} />
-                <meta property="og:type" content='website' />
-                <meta name="twitter:card" content="summary_large_image" />
-                <meta name="twitter:title" content={title} />
-                <meta name="twitter:description" content={description} />
-                <meta name="twitter:image" content={imageUrl} />
-                <meta name="description" content={description} />
-
-            </Head>
-
             <div className='page-bg'>
                 <div className='position-relative h-100'>
                     <div className='vertical-center verify-cert'>
